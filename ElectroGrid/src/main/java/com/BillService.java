0d6b5f95624c7ab83571a91b1045459a7d8fe085
Madhushaa <madushahanshini1@gmailcom>
2022-04-24 21:09:40 +0530

    implement retrive Bill Service

--- ElectroGrid/src/main/java/com/BillService.java
+++ ElectroGrid/src/main/java/com/BillService.java
@@ -6,13 +6,6 @@ import model.Bill;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-//For JSON
-import com.google.gson.*;
-
-//For XML
-import org.jsoup.*;
-import org.jsoup.parser.*;
-import org.jsoup.nodes.Document;
 
 @Path("/Bills")
 public class BillService {
