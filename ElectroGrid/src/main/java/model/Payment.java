b74702b8bbb6ee395c278fc962e7b9228449807a
chithmi <chithmi99suhara@gmail.com>
2022-04-24 11:05:43 +0530

    Create Database Connection

--- ElectroGrid/src/main/java/model/Payment.java
+++ ElectroGrid/src/main/java/model/Payment.java
@@ -1,5 +1,25 @@
 package model;
 
-public class Payment {
+import java.sql.*;
+import java.text.SimpleDateFormat; 
 
+public class Payment {
+	
+	 //Connect to the DB
+	 private Connection connect()
+	 {
+	 Connection con = null;
+	 try
+	 {
+	 Class.forName("com.mysql.cj.jdbc.Driver");
+	
+	 //Provide the correct details: DBServer/DBName, user name, password
+	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/payment", "root", "");
+	 }
+	 catch (Exception e)
+	 {e.printStackTrace();}
+	 return con;
+
+}
+	 
 }
