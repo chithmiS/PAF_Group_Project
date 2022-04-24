258590f9a0f6a8b2767d5aa9143faaa22e7c1254
chithmi <chithmi99suhara@gmail.com>
2022-04-24 11:21:22 +0530

    Create insertPayment method

--- ElectroGrid/src/main/java/com/PaymentService.java
+++ ElectroGrid/src/main/java/com/PaymentService.java
@@ -1,5 +1,39 @@
 package com;
 
+import model.Payment;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+
+@Path("/Payments") 
+
 public class PaymentService {
+	
+		Payment paymentObj = new Payment();
+	
+		//add payment details
+	
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String insertPayment(@FormParam("AccNumber") String AccNumber,
+		 @FormParam("totalAmount") String totalAmount,
+		 @FormParam("payDate") String payDate,
+		 @FormParam("cardType") String cardType,
+		 @FormParam("cardNumber") String cardNumber,
+		 @FormParam("cvn") String cvn)
+		{
+		 String output = paymentObj.insertPayment(AccNumber, totalAmount, payDate, cardType, cardNumber, cvn);
+		return output;
+		}
 
 }
--- ElectroGrid/src/main/java/model/Payment.java
+++ ElectroGrid/src/main/java/model/Payment.java
@@ -21,5 +21,54 @@ public class Payment {
 	 return con;
 
 }
+
+public String insertPayment(String AccNumber, String totalAmount, String payDate, String cardType, String cardNumber, String cvn)
+	 {
+		 String output = "";
+		 try
+		 {
+		 Connection con = connect();
+		 
+		 java.util.Date dateObj;
+		 
+		 if (con == null)
+			 {return "Error while connecting to the database for inserting."; }
+		 
+			 // create a prepared statement
+		 
+			 String query = " insert into payments(`payID`,`AccNumber`,`totalAmount`,`payDate`,`cardType`,`cardNumber`,`cvn`)"
+			 + " values (?, ?, ?, ?, ?, ?, ?)";
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 
+			 preparedStmt.setInt(1, 0);
+			 preparedStmt.setString(2, AccNumber);
+			 preparedStmt.setDouble(3, Double.parseDouble(totalAmount));
+			
+			 SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+			 dateObj = format.parse(payDate);
+			 preparedStmt.setDate(4, new java.sql.Date(dateObj.getTime()));
+			 
+			 
+			 preparedStmt.setString(5, cardType);
+			 preparedStmt.setString(6, cardNumber);
+			 preparedStmt.setString(7, cvn);
+			 
+			 // execute the statement
+			 
+			 preparedStmt.execute();
+			 con.close();
+			 output = "Inserted successfully";
+		 }
+		 
+		 catch (Exception e)
+		 {
+			 output = "Error while inserting the item.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output;
+	 }
+
 	 
 }
